2966003472468f02c7c10a4f4a953dee9cb34fdb
nervig <solide@yandex.ru>
2021-03-12 09:51:17 +0300

    feat: add code of function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = n / -1;
+    }
+    let str_num = String(n);
+    str_num = str_num.split('').reverse().join('');
+    str_num = Number(str_num);
+
+    return str_num;
 }
